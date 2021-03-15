7d189762c06c0136eaa6e5c2f896d0975fa7ae3e
Oriane Tioni <o.tioni@cci-formation.nc>
2021-03-15 11:20:22 +1100

    Update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+//exo 2
+    let name = 'John';
+    let lastName = 'Doe';
+    let age = 32;
+    alert('Name: ' + Name + '\n'
+      + 'LastName: ' + lastName + '\n'
+      + 'Age: ' + age);
+
+
+
