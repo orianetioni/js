024b8496f3adfcc9c0581e84848d3bec688b5621
Oriane Tioni <o.tioni@cci-formation.nc>
2021-03-15 22:21:50 +1100

    exo8

---
+++ exo-8/main.js
@@ -0,0 +1,13 @@
+function yourAge() 
+{
+    let age=document.getElementById('age').value;
+     if(age >= 18)
+     {
+         alert('Vous êtes majeure');
+     }
+     else 
+     {
+         alert('Vous êtes mineure');
+     }
+
+}
