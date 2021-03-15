7cab05befd6f5174d514e2c990527b36daa2dffc
Oriane Tioni <o.tioni@cci-formation.nc>
2021-03-15 21:30:14 +1100

    exo6

---
+++ exo-6/main.js
@@ -0,0 +1,10 @@
+ function resteDivision(){
+     
+    let firstNumber= document.getElementById('firstNumber').value;
+    let secondNumber= document.getElementById('secondNumber').value;
+    let result= (firstNumber % secondNumber);
+    
+    alert("Le reste de la division est: " + result);
+   
+
+ }
