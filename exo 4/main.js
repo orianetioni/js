0ddca7eba715c51ae18e744120a6d6f66e44eec3
Oriane Tioni <o.tioni@cci-formation.nc>
2021-03-15 14:42:48 +1100

    exo4

---
+++ exo 4/main.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    var x1=document.getElementById('lastname').value;
+    var x2=document.getElementById('firstname').value;
+    var x3=document.getElementById('city').value;
+   
+    alert ('Nom: ' + x1 + '\n'
+    + 'prénom: ' + x2 + '\n'
+    + 'Ville: ' + x3);
+   }
