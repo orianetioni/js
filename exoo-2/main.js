539e5e2c1c60dd6fd9d004576bcaf5af56b95602
Oriane Tioni <o.tioni@cci-formation.nc>
2021-03-15 14:50:43 +1100

    rename

--- exoo-2/main.js
+++ exoo-2/main.js
@@ -1,10 +1,10 @@
 //exo 2
-    let name = 'John';
+    let Name = 'John';
     let lastName = 'Doe';
     let age = 32;
     alert('Name: ' + Name + '\n'
       + 'LastName: ' + lastName + '\n'
       + 'Age: ' + age);
 
-
+      
 
